6538529c8147794a07c69ba26d9d9430a43c5b94
Chris Kelly <chris430@Chriss-MacBook-Air-2.local>
2026-03-24 12:01:57 +0000

    Tidied Up Utills.py file

--- utils.py
+++ utils.py
@@ -1,29 +1,5 @@
-import tensorflow as tf
 import numpy as np
 import torch
-
-## Old version
-##def construct_input_tensor(Y, Xp):
-##    # Re-Size input tensors
-##    Hr = Y * np.conj(Xp)
-##    Y = tf.squeeze(Y, axis=[0, 1])
-##    Xp = tf.expand_dims(Xp, axis=0)
-##    Hr = tf.squeeze(Hr, axis=[0, 1])
-##
-##    # Combine input Tenosrs into one Input
-##    Z = tf.concat([Y, Xp, Hr], axis=0)
-##    Z = tf.concat([tf.math.imag(Z), tf.math.real(Z)], axis=0)
-##
-##    # Add batch dimension for batch training
-##    # Z = tf.expand_dims(Z, axis=0)  # → (1, 10, 14, 512)
-##
-##
-##    # Convert Input Tensor from Tensor Flow to PyTorch
-##    Z = tf.cast(Z, tf.float32)  # ensure float32
-##    Z = torch.tensor(Z.numpy())  # TF → PyTorch tensor
-##
-##    return Z
-
 
 def construct_input_tensor(Y, Xp):
     # Compute raw channel estimate
